2b37f8372041c99d8bed4854e47dff477225bd03
naakrong-adjei <naaadjorkorakrongadjei@gmail.com>
2025-08-09 02:26:37 +0000

    Added Task1 (Inheritance and method overriding)

---
+++ Task1_Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task1_Inheritance
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+
+        }
+    }
+}
