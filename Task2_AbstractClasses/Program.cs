c6de510080b4ea2e1079d3809586a7c87aee274c
naakrong-adjei <naaadjorkorakrongadjei@gmail.com>
2025-08-09 02:48:51 +0000

    Added Task2 (Abstract classes)

---
+++ Task2_AbstractClasses/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task2_AbstractClasses
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return 3.14 * radius * radius;
+        }
+    }
+
+
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(15);
+            Shape rectangle = new Rectangle(100, 56);
+
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        }
+    }
+}
